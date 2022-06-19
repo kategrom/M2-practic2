3d8ef7e717ca295e3fa2c33335100044adc953c4
kategrom <95121842+kategrom@users.noreply.github.com>
2022-06-19 11:08:08 +0300

    Add files via upload

---
+++ M2-practic2.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// task 1
+
+// function hello1() {
+//   return "Привіт JavaScript";
+// }
+
+// console.log(hello1());
+
+
+
+// task 2
+
+// function hello2(name) {
+//   return 'Привіт, ' + name; 
+// }
+
+// console.log(hello2("Василь"));
+
+
+// task 3
+
+
+// function num(m, n) {
+//   console.log(m * n);
+//   console.log(m + n);
+//   console.log(m - n);
+// }
+
+// num(3, 1);
+
+
+// task 4   
+
+// const myNewArrowFunction = (...args) => {
+//   cons ole.log(args);
+// };
+
+// myNewArrowFunction(1, 2, 3);
+// myNewArrowFunction(100, 200, 300, 400, 500);
+// myNewArrowFunction("Js", "Python", "Java", "PHP", "C++");
+
+
+// task 5
+
+// function myAverageScore(array) {
+//   let scoreSum = 0;
+//   for (const element of array) {
+//     scoreSum += element;
+//   }
+
+//   const average = scoreSum / array.length;
+
+//   if (average <= 100 && average >= 91){
+//     return "Ваша оцінка A";
+//   } else if (average <= 90 && average >= 81){
+//     return "Ваша оцінка B";
+//   } else if (average <= 80 && average >= 71){
+//     return "Ваша оцінка C";
+//   } else if (average <= 70){
+//     return "Ваша оцінка D";
+//   } 
+// }
+
+// console.log(myAverageScore([100, 75, 81, 96]));
+// console.log(myAverageScore([45, 63, 85, 70]));
+// console.log(myAverageScore([77, 82, 60, 58]));
+// console.log(myAverageScore([93, 99, 93, 96]));
